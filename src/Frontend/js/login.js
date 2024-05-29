39e33955cce3ff9e5acc2bd0054771b2bb582fea
G-Lungov <gabriellungov2001@gmail.com>
2024-05-29 11:22:42 -0300

    Set up API 005 (SecretKey and EventListener login.js)

--- src/Frontend/js/login.js
+++ src/Frontend/js/login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;

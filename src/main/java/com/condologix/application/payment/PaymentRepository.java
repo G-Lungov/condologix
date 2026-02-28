efd0f53145049c700b77cd0f696a723de5ca9030
G-Lungov <gabriellungov2001@gmail.com>
2026-02-28 13:34:25 -0300

    Created basic methods to payment repository

--- src/main/java/com/condologix/application/payment/PaymentRepository.java
+++ src/main/java/com/condologix/application/payment/PaymentRepository.java
@@ -1,5 +1,13 @@
 package com.condologix.application.payment;
 
-public class PaymentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentModel, Long> {
+
+    List<PaymentModel> findByBuilding (Long buildingId);
+    List<PaymentModel> findByStatus (PaymentStatus status);
+    List<PaymentModel> findByBuildingAndStatus (Long buildingId, PaymentStatus status);
 }

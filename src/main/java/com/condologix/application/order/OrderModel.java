e05c5aa3d41f2caa38f08d086e831f7491a26ddd
G-Lungov <gabriellungov2001@gmail.com>
2026-03-13 22:00:42 -0300

    Added methods and validations inside OrderModel.java

--- src/main/java/com/condologix/application/order/OrderModel.java
+++ src/main/java/com/condologix/application/order/OrderModel.java
@@ -22,16 +22,16 @@ public class OrderModel {
 
     @ManyToOne(optional = false)
     @JoinColumn(name =  "BUILDING_ID", nullable = false)
-    private BuildingModel buildingId;
+    private BuildingModel building;
     @ManyToOne(optional = false)
-    @JoinColumn(name = "NAME_ID", nullable = false)
-    private UnitModel unitId;
+    @JoinColumn(name = "UNIT_ID", nullable = false)
+    private UnitModel unit;
     @ManyToOne(optional = false)
     @JoinColumn(name = "RESIDENT_ID", nullable = false)
-    private ResidentModel residentId;
+    private ResidentModel resident;
     @ManyToOne(optional = false)
     @JoinColumn(name = "CONCIERGE_ID", nullable = false)
-    private ConciergeModel conciergeId;
+    private ConciergeModel concierge;
 
     @Column(name = "SENDER_NAME", nullable = false, length = 50)
     private String senderName;
@@ -42,37 +42,71 @@ public class OrderModel {
     @Column(name = "DESCRIPTION", nullable = true, length = 100)
     private String description;
 
-    @Column(name = "ARRIVED_AT")
+    @Column(name = "ARRIVED_AT", nullable = false)
     private LocalDateTime arrivedAt;
     @Column(name = "NOTIFIED_AT")
     private LocalDateTime notifiedAt;
-    @Column(name = "PICED_UP_AT")
+    @Column(name = "PICKED_UP_AT")
     private LocalDateTime pickedUpAt;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "REGISTRATION_TYPE")
+    @Column(name = "REGISTRATION_TYPE", nullable = false)
     private OrderRegistrationType registrationType;
     @Enumerated(EnumType.STRING)
-    @Column(name = "STATUS")
+    @Column(name = "STATUS", nullable = false)
     private OrderStatus status;
 
     public OrderModel (
-        BuildingModel buildingId,
-        UnitModel unitId,
-        ResidentModel residentId,
-        ConciergeModel conciergeId,
+        BuildingModel building,
+        UnitModel unit,
+        ResidentModel resident,
+        ConciergeModel concierge,
         String senderName,
         String carrier,
         String trackingCode,
-        String description
+        String description,
+        OrderRegistrationType registrationType
     ) {
-        this.buildingId = buildingId;
-        this.unitId = unitId;
-        this.residentId = residentId;
-        this.conciergeId = conciergeId;
+        if (building == null) throw new IllegalArgumentException("Building cannot be null");
+        if (unit == null) throw new IllegalArgumentException("Unit cannot be null");
+        if (resident == null) throw new IllegalArgumentException("Resident cannot be null");
+        if (concierge == null) throw new IllegalArgumentException("Concierge cannot be null");
+        if (senderName ==  null || senderName.isBlank()) throw new IllegalArgumentException("Sender name cannot be null or blank");
+        if (carrier == null || carrier.isBlank()) throw new IllegalArgumentException ("Carrier cannot be null or blanck");
+
+        this.building = building;
+        this.unit = unit;
+        this.resident = resident;
+        this.concierge = concierge;
         this.senderName = senderName;
         this.carrier = carrier;
         this.trackingCode = trackingCode;
         this.description = description;
+        this.registrationType = registrationType;
+        this.arrivedAt = LocalDateTime.now();
+        this.status = OrderStatus.RECEIVED;
     }
+
+    public void markAsNotified() {
+        if (this.status != OrderStatus.RECEIVED) {
+            throw new IllegalArgumentException("Only orders with status RECEIVED can be marked as notified");
+        }
+        this.status = OrderStatus.NOTIFIED;
+        this.notifiedAt = LocalDateTime.now();
+    }
+
+    public void markAsPickedUp() {
+        if (this.status == OrderStatus.PICKED_UP) {
+            throw new IllegalArgumentException("Order is already macked as picked up");
+        }
+    }
+
+    public void changeResident(ResidentModel newResident, UnitModel newUnit) {
+        if (this.status == OrderStatus.PICKED_UP) {
+            throw new IllegalArgumentException("Cannot change resident for an order that has already been picked up");
+        }
+        this.unit = newUnit;
+        this.resident = newResident;
+    }
+
 }

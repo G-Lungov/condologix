2e6be88d1baa7da799d96c72b3f39ac27ef182d0
G-Lungov <gabriellungov2001@gmail.com>
2026-03-18 19:39:19 -0300

    Coding OrderService

--- src/main/java/com/condologix/application/order/OrderService.java
+++ src/main/java/com/condologix/application/order/OrderService.java
@@ -1,5 +1,66 @@
 package com.condologix.application.order;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.condologix.application.building.BuildingModel;
+import com.condologix.application.concierge.ConciergeModel;
+import com.condologix.application.resident.ResidentModel;
+import com.condologix.application.unit.UnitModel;
+
+@Service
+@Transactional
 public class OrderService {
 
+    private final OrderRepository orderRepository;
+    public OrderService (OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    private void notifyResident(OrderModel order) {
+        // Integrate with notification.
+    }
+
+    public OrderModel createOrder(
+        BuildingModel building,
+        UnitModel unit,
+        ResidentModel resident,
+        ConciergeModel concierge,
+        String senderName,
+        String carrier,
+        String trackingCode,
+        String description,
+        OrderRegistrationType registrationType
+    )  {
+        OrderModel order = new OrderModel(
+            building,
+            unit,
+            resident,
+            concierge,
+            senderName,
+            carrier,
+            trackingCode,
+            description,
+            registrationType
+        );
+
+        OrderModel savedOrder = orderRepository.save(order);
+        if (resident != null) {
+            notifyResident(savedOrder);
+            savedOrder.markAsNotified();
+        }
+
+        return savedOrder;
+
+    }
+
+    public OrderModel assignResident(Long orderId, ResidentModel resident) {
+        OrderModel order = orderRepository.findById(orderId)
+            .orElseThrow(() -> new IllegalArgumentException("Order not found with id: " + orderId));
+        order.assignResident(resident);
+        notifyResident(order);
+        order.markAsNotified();
+
+        return orderRepository.save(order);
+    }
 }

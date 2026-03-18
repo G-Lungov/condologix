223cab9f05fba333e5fed78ee75593e4ff1a8097
G-Lungov <gabriellungov2001@gmail.com>
2026-03-18 18:14:27 -0300

    OrderService complete

--- src/main/java/com/condologix/application/order/OrderRepository.java
+++ src/main/java/com/condologix/application/order/OrderRepository.java
@@ -1,5 +1,17 @@
 package com.condologix.application.order;
 
-public class OrderRepository {
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<OrderModel, Long>{
+
+    List<OrderModel> findByBuildingId(Long buildingId);
+    List<OrderModel> findByBuildingAndStatus(Long buildingId, OrderStatus status);
+    List<OrderModel> findByResidentId(Long residentId);
+    List<OrderModel> findByPendingResidenIdAndStatus(Long residentId, OrderStatus status);
+    List<OrderModel> findByBuildingIdAndResidentIsNull(Long buildingId);
+    Optional<OrderModel> findByTrackingCode(String trackingCode);
 
 }

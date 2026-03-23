9247ecfe77e74004b396694ff1c97d3e70c82e20
G-Lungov <gabriellungov2001@gmail.com>
2026-03-23 19:40:11 -0300

    Modeling BuldingModel and adjusting import inside OrderController

--- src/main/java/com/condologix/application/building/BuildingModel.java
+++ src/main/java/com/condologix/application/building/BuildingModel.java
@@ -14,14 +14,45 @@ public class BuildingModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(name = "NAME", nullable = false)
     private String name;
+    @Column(name = "PHONE", nullable = false)
     private long phone;
+    @Column(name = "EMAIL", nullable = false)
     private String email;
+    @Column(name = "ADDRESS", nullable = false)
     private String address;
+    @Column(name = "ADDRESS_NUMBER", nullable = false)
     private int addressNumber;
+    @Column(name = "POSTAL_CODE", nullable = false)
     private int postalCode;
+    @Column(name = "NEIGHBORHOOD", nullable = false)
     private String neighborhood;
+    @Column(name = "CITY", nullable = false)
     private String city;
+    @Column(name = "STATE", nullable = false)
     private String state;
 
+    public BuildingModel (
+        String name,
+        long phone,
+        String email,
+        String address,
+        int addressNumber,
+        int postalCode,
+        String neighborhood,
+        String city,
+        String state
+    ) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.addressNumber = addressNumber;
+        this.postalCode = postalCode;
+        this.neighborhood = neighborhood;
+        this.city = city;
+        this.state = state;
+    }
 }
--- src/main/java/com/condologix/application/order/OrderController.java
+++ src/main/java/com/condologix/application/order/OrderController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.condologix.application.resident.ResidentModel;
-
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {

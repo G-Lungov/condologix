45cdc76a2e47eded93624fccd90c58606eaa1beb
G-Lungov <gabriellungov2001@gmail.com>
2026-04-21 00:30:16 -0300

    Updating UnitService to be possible change Unit Type

--- src/main/java/com/condologix/application/unit/UnitService.java
+++ src/main/java/com/condologix/application/unit/UnitService.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.condologix.application.building.BuildingModel;
 import com.condologix.application.building.BuildingRepository;
+import com.condologix.application.exception.ResourceNotFoundException;
 
 @Service
 @Transactional
@@ -14,21 +15,31 @@ public class UnitService {
 
     private final UnitRepository unitRepository;
     private final BuildingRepository buildingRepository;
+
     public UnitService(UnitRepository unitRepository, BuildingRepository buildingRepository) {
         this.unitRepository = unitRepository;
         this.buildingRepository = buildingRepository;
     }
 
+    // UnitDTO createUnit
     public UnitModel createUnit(Long buildingId, short number, String block, UnitType unitType) {
         BuildingModel building = buildingRepository.findById(buildingId)
-            .orElseThrow(() -> new IllegalArgumentException("Building not found with id: " + buildingId));
-            String normalizedBlock = normalizedBlock(block);
+            .orElseThrow(() -> new ResourceNotFoundException("Building not found with id: " + buildingId));
+        String normalizedBlock = normalizedBlock(block);
 
         if (unitRepository.existsByBuildingIdAndBlockAndNumber(building.getId(), normalizedBlock, number)) {
             throw new IllegalArgumentException("Unit with the same block and number already exists");
         }
 
         UnitModel unit = new UnitModel(building, number, normalizedBlock, unitType);
+        return unitRepository.save(unit);
+    }
+
+    // UnitDTO updateUnitType
+    public UnitModel updateUnitType(Long unitId, UnitType unitType) {
+        UnitModel unit = unitRepository.findById(unitId)
+            .orElseThrow(() -> new ResourceNotFoundException("Unit not found with id: " + unitId));
+        unit.updateUnitType(unitType);
         return unitRepository.save(unit);
     }
 

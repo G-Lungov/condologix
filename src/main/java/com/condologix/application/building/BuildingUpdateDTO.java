246c877b283304300097c5b9b93e960e058c5984
G-Lungov <gabriellungov2001@gmail.com>
2026-05-02 15:53:37 -0300

    BuildingUpdateDTO coded

--- src/main/java/com/condologix/application/building/BuildingUpdateDTO.java
+++ src/main/java/com/condologix/application/building/BuildingUpdateDTO.java
@@ -1,5 +1,17 @@
 package com.condologix.application.building;
 
-public class BuildingUpdateDTO {
+import jakarta.validation.constraints.*;
+
+public record BuildingUpdateDTO(
+    @NotBlank(message = "Phone contact is required, only digits")
+    @Size(min = 11, max = 11, message = "Phone must have 11 digits")
+    @Pattern(regexp = "^\\d{11}$", message = "Phone must have exactly 11 digits")
+    String phone,
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email must be a valid email address")
+    @Size(max = 100, message = "Email must be at most 100 characters")
+    String email
+) {
 
 }

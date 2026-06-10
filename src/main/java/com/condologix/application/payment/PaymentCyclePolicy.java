3f851129c893b75d0642bee484c93deef60f91c2
G-Lungov <gabriellungov2001@gmail.com>
2026-06-10 07:23:04 -0300

    Adjusted PaymentCyclePolicy adding calculateBillingPeriod method

--- src/main/java/com/condologix/application/payment/PaymentCyclePolicy.java
+++ src/main/java/com/condologix/application/payment/PaymentCyclePolicy.java
@@ -1,12 +1,20 @@
 package com.condologix.application.payment;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.temporal.TemporalAdjusters;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PaymentCyclePolicy {
 
     public boolean isGenerationDate(LocalDate date) {
         return date.equals(date.with(TemporalAdjusters.lastDayOfMonth()));
+    }
+
+    public String calculateBillingPeriod(LocalDate generationDate) {
+        return YearMonth.from(generationDate.plusMonths(1)).toString();
     }
 
     public LocalDate calculateDueDate(LocalDate generationDate) {
